a133442d6d4995605ae51189299077188896ad2d
svafiadis1 <57498741+svafiadis1@users.noreply.github.com>
2020-04-06 23:49:07 -0400

    challenges

--- .vscode/index.js
+++ .vscode/index.js
@@ -23,6 +23,7 @@
      pets: ["cat", "dog"]
  }; */
 
+ /*
  const Haoran = {
      name: "how",
      dob: 2001,
@@ -31,9 +32,61 @@
          return 2020 - this.dob;
      }
 };
+*/
 /* 
  const Eric = Haoran.year; //reference to Haoran
  Eric.name = "Eric";
  const Christian = {};
  Christian.dob = Haoran.dob
  Christian.dob = 2003; */
+
+ const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  ];
+   
+  //Challenge Questions
+  //1) Filter all presidents, leaving only the Democratic ones
+/*
+  const Democrats =  presidents.filter(function(president) {
+	return president.party == "D";
+});
+console.log(Democrats);
+*/
+   
+  //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+
+  /*
+  const oneTerm =  presidents.filter(function(president) {
+    if (president.party == "R"){
+    return president.terms == "1";
+    }
+  })
+  
+  console.log(oneTerm);
+  */
+   
+  //3) return only the last three presidents
+  /*
+  let lastThree = presidents.slice(9, 12);
+  console.log(lastThree);
+  */
+
+  //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+  const twoTermDem =  presidents.filter(function(president) {
+    if (president.party == "D"){
+    return president.terms == "2";
+    }
+  }) 
+  console.log(twoTermDem);
+  //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
